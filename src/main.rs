6e5e92f4b765e417c0c651cde76fe270c5e4d52e
Varun Maheshwari <Maheshwari.varun753@gmail.com>
2025-05-10 06:07:34 +0530

    Added the delete functionality, also changed the logic to generate ids for the task and applied some error handling part too

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdin};
+use std::io::{self};
 
 fn main() {
     const OPTIONS_LIST: &str = r#" Welcome to TO-DO List CLI APPLICATION 
@@ -19,28 +19,27 @@ fn main() {
     //     load
     // }
     let mut tasks_list = Vec::new();
-
+    let mut id_counter: u32 = 0;
     loop {
         println!("{OPTIONS_LIST}");
         let mut command = String::new();
         let _ = io::stdin().read_line(&mut command);
-        println!("command: {command}");
+        println!("\n");
+        // println!("command: {command}");
         
         if command.trim().to_lowercase().as_str()=="add" {
-            add_a_task(&mut tasks_list)
+            add_a_task(&mut tasks_list, id_counter);
+            id_counter = id_counter + 1;
         } else if command.trim().to_lowercase().as_str()=="view" {
             view_all_tasks(&tasks_list)
         } else if command.trim().to_lowercase().as_str()=="update" {
             update_a_task(&mut tasks_list)
         } else if command.trim().to_lowercase().as_str()=="delete" {
-            delete_a_task()
-        } else if command.trim().to_lowercase().as_str()=="export" {
-            export_a_task()
+            delete_a_task(&mut tasks_list)
         } else {
-            load_a_task()
+            println!("Didn't recognise: {}, please enter commands from the option shown:\n", command.trim())
         }
     }
-
 }
 
 enum Status {
@@ -55,14 +54,14 @@ struct TaskFormat {
 }
 
 
-fn add_a_task(tasks_store: &mut Vec<TaskFormat>) {
+fn add_a_task(tasks_store: &mut Vec<TaskFormat>, counter: u32) {
     const REQUEST_FOR_DESC: &str = r#"Please enter the task description."#;
     println!("{REQUEST_FOR_DESC}");
 
     let mut task_description = String::new();
     let _ = io::stdin().read_line(&mut task_description);
 
-    let id = {(tasks_store.len()+1).try_into().unwrap()};
+    let id = counter + 1;
     tasks_store.push(TaskFormat{
         id,
         description: task_description,
@@ -71,6 +70,7 @@ fn add_a_task(tasks_store: &mut Vec<TaskFormat>) {
     println!("Task Added Successfully!")
 
 }
+
 
 fn view_all_tasks(tasks_store: &Vec<TaskFormat>) {
     
@@ -87,14 +87,23 @@ fn view_all_tasks(tasks_store: &Vec<TaskFormat>) {
 
 }
 
+
 fn update_a_task(tasks_store: &mut Vec<TaskFormat>) {
     println!("Please enter the task ID to mark it as complete");
     let mut task_id = String::new();
     let _ = io::stdin().read_line(&mut task_id);
 
+    let parsed_id = match task_id.trim().parse::<u32>(){
+        Ok(id) => id,
+        Err(_) => {
+            println!("Please enter a valid task ID");
+            return;
+        }
+    };
+
     //First check if the ID exists or not
     for task in tasks_store {
-        if task.id==task_id.trim().parse().unwrap(){
+        if task.id==parsed_id{
             let description = &task.description;
             task.status = match task.status {
                 Status::Completed => {
@@ -112,14 +121,24 @@ fn update_a_task(tasks_store: &mut Vec<TaskFormat>) {
 }
 
 
-fn delete_a_task() {
-    println!("deleting a task")
-}
+fn delete_a_task(tasks_store: &mut Vec<TaskFormat>) {
 
-fn export_a_task() {
-    println!("exporting a task")
-}
-
-fn load_a_task() {
-    println!("loading a task")
+    println!("Please provide the task id to delete:");
+    let mut to_delete_task_id: String = String::new();
+    let _ = io::stdin().read_line(&mut to_delete_task_id);
+    
+    let parsed_id = match to_delete_task_id.trim().parse::<u32>() {
+        Ok(id) => id,
+        Err(_) => {
+            println!("Invalid Task Id. Please check the task id");
+            return;
+        }
+    };
+    
+    if let Some(index) = tasks_store.iter().position(|task| task.id==parsed_id){
+        let removed_task = tasks_store.remove(index);
+        println!("Deleted task {}: {}", removed_task.id, removed_task.description);
+    } else {
+        println!("No task found with ID: {}", to_delete_task_id);
+    }
 }
